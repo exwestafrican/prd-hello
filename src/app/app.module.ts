86667d8a0204da5d9d000e3d38130f9dbe21e8fa
summer <oladele.alade@gmail.com>
2022-01-09 12:59:10 +0000

    ch: generate shop component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ShopComponent } from './pages/shop/shop.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AboutComponent } from './pages/about/about.component';
     CarouselComponent,
     HeroComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
