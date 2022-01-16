0d4f9411a702024f9d657eee66c3e75979e59acb
summer <oladele.alade@gmail.com>
2022-01-16 05:33:26 +0000

    ch: added path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { ShopComponent } from './pages/shop/shop.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { "path": "", "component": HomeComponent },
+  { "path": "about", "component": AboutComponent },
+  { "path": "shop", "component": ShopComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
